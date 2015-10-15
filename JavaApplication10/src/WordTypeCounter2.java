88bf2a903b7f87315950052ac1427de7ef9ec5ee
cridlcas <crisdlcas@hotmail.com>
2015-10-14 21:23:04 -0600

    Agregación de instrucciones para usuario.
    Este se puede correr desde NetBeans.

--- JavaApplication10/src/WordTypeCounter2.java
+++ JavaApplication10/src/WordTypeCounter2.java
@@ -1,14 +1,16 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////HOJA DE TRABAJO 9///////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////14482////////////////////////FERNANDA DAVILA//////////////////////
+////////////////////////14015////////////////////////CRISTIAN DE LEON/////////////////////
+////////////////////////14069////////////////////////MICHEL RAMIREZ///////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////ESTE MAIN SE PUEDE CORRER DESDE NETBEANS, EL OTRO SOLO DESDE CMD//
+////////////////////////HAY QUE MODIFICAR LAS DIRECCIONES DEPENDIENDO DE LA COMPU/////////
+////////////////////////EL ARGS2 SE DEBE CAMBIAR ENTRE 1 Y 5//////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 
-/*
-UVG
-Algoritmos y Estructuras de Datos - 2011
-Hoja de trabajo 7
-Autor: Eduardo Castellanos
-
-Descripci�n: Programa principal. 
-*/
 import java.io.*;
-
 class WordTypeCounter {
 	public static void main(String[] args) throws Exception{
             String[] args2 = new String[3];
@@ -17,69 +19,53 @@ class WordTypeCounter {
             args2[2] = "5";
             
 		if(args2.length > 1){
-			// Declaraci�n e inicializaci�n de variables.
-			// el primer parametro indica el nombre del archivo con las definiciones de las palabras
 			File wordFile = new File(args2[0]);
-			
-			// el segundo parametro indica el nombre del archivo que tiene el texto a analizar
 			File textFile = new File(args2[1]);
 			
-			// el tercer parametro sirve para seleccionar la implementacion que se usara para
-			// guardar el conjunto de palabras. Use el valor 1 para indicar que se empleara
-			// la implementacion SimpleSet que acompa�a esta tarea.
-			// Para el resto de implementaciones:
-                        
-                        //  1 SimpleSet
+			///////////////////////////////////////////////////////////////////
+			//  1 SimpleSet
 			//  2 Red Black Tree
 			//  3 Splay Tree
 			//  4 Hash Table
 			//  5 TreeMap (de java collection framework)
-			int implementacion = Integer.parseInt(args2[2]);
+			///////////////////////////////////////////////////////////////////
 			
+			int implementacion = Integer.parseInt(args2[2]);
 			BufferedReader wordreader;
 			BufferedReader textreader;
-			
 			int verbs=0;
 			int nouns=0;
 			int adjectives=0;
 			int adverbs=0;
 			int gerunds=0;
-			
 			long starttime;
 			long endtime;
 			
-			// Verificar que los dos par�metros que se pasaron sean archivos que existen
+			//VERIFICA QUE PARAMETROS EXISTEN
 			if(wordFile.isFile() && textFile.isFile()) {
 				// Leer archivos
-				try
-				{
+				try{
 					wordreader = new BufferedReader(new FileReader(wordFile));
 					textreader = new BufferedReader(new FileReader(textFile));
 				}
-				catch (Exception ex)
-				{
+				catch (Exception ex){
 					System.out.println("Error al leer!");
 					return;
 				}
 
-				// Crear un WordSet para almacenar la lista de palabras
-				// se selecciona la implementacion de acuerdo al tercer parametro pasado en la linea
-				// de comando
-				// =====================================================
+				///////////////////////////////////////////////////////////////////
 				WordSet words =  WordSetFactory.generateSet(implementacion);
-				// =====================================================
+				///////////////////////////////////////////////////////////////////
 				
 				String line = null;
 				String[] wordParts;
 				
-				// leer el archivo que contiene las palabras y cargarlo al WordSet.
+				//LEE ARCHIVO
 				starttime = System.currentTimeMillis();
 				line = wordreader.readLine();
-				while(line!=null)
-				{
+				while(line!=null){
 					wordParts = line.split("\\.");  // lo que esta entre comillas es una expresi�n regular.
-					if(wordParts.length == 2)
-					{
+					if(wordParts.length == 2){
 						words.add(new Word(wordParts[0].trim(),wordParts[1].trim()));
 					}
 					line = wordreader.readLine();
@@ -89,25 +75,21 @@ class WordTypeCounter {
 				
 				System.out.println("Palabras cargadas en " + (endtime-starttime) + " ms.");
 				
-				// Procesar archivo de texto
 				starttime = System.currentTimeMillis();
 				line = textreader.readLine();
 				String[] textParts;
 				Word currentword;
 				Word lookupword = new Word();
 				
-				while(line!=null)
-				{
-					// Separar todas las palabras en la l�nea.
-					textParts = line.split("[^\\w-]+"); // utilizar de separador cualquier caracter que no sea una letra, n�mero o gui�n.
+				while(line!=null){
+					// SEPARA PALABRAS
+					textParts = line.split("[^\\w-]+");
 					
-					// Revisar cada palabra y verificar de que tipo es. 
-					for(int i=0;i<textParts.length;i++)
-					{
+					// VERIFICA EL TIPO 
+					for(int i=0;i<textParts.length;i++){
 						lookupword.setWord(textParts[i].trim().toLowerCase());
 						currentword = words.get(lookupword);
-						if(currentword != null)
-						{
+						if(currentword != null){
 							if(currentword.getType().equals("v-d") || currentword.getType().equals("v") || currentword.getType().equals("q"))
 								verbs++;
 							else if(currentword.getType().equals("g") )
@@ -127,22 +109,20 @@ class WordTypeCounter {
 				endtime = System.currentTimeMillis();
 				System.out.println("Texto analizado en " + (endtime-starttime) + " ms.");
 				
-				// Presentar estad�sticas
+				///////////////////////////////////////////////////////////////////
 				System.out.println("El texto tiene:");
 				System.out.println(verbs + " verbos");
 				System.out.println(nouns + " sustantivos");
 				System.out.println(adjectives + " adjetivos");
 				System.out.println(adverbs + " adverbios");
 				System.out.println(gerunds + " gerundios");
-				
+				///////////////////////////////////////////////////////////////////
 			}
-			else
-			{
+			else{
 				System.out.println("No encuentro los archivos :'( ");
 			}
 		}
-		else
-		{
+		else{
 			System.out.println("Faltan Parametros.");
 		}
 	}
